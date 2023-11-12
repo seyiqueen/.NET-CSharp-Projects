b317fe06947a6a85eba83e2e8da03fe0db52b054
Oluwaseyi Sodipo <sodiposeyi@gmail.com>
2023-11-12 12:02:28 +0000

    Simple CSharp App

---
+++ SimpleCSharpApp/SimpleCSharpApp/Program.cs
@@ -0,0 +1,13 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        // Display a simple message to the user.
+        Console.WriteLine("***** My First C# App *****");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine();
+        // Wait for Enter key to be pressed before shutting down.
+        Console.ReadLine();
+    }
+}
+
